e34406be79ad4dd70c3a3988ff952498a288fe03
Scott Morgan <MorganSN@cardiff.ac.uk>
2018-11-26 14:22:22 +0000

    amended Bridgend College test page

--- _pages/test2/app-files/data.js
+++ _pages/test2/app-files/data.js
@@ -37,6 +37,6 @@ var APP_DATA = {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": true
+    "viewControlButtons": false
   }
 };
